b0a2bfd86605e65e549c0351dffca04308304e2e
Manav Sharma <ishmanav@gmail.com>
2022-05-02 19:17:44 +1000

    Initial commit

---
+++ src/main/java/com/mjs/YummyPizzaRestaurant/YummyPizzaRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.mjs.YummyPizzaRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyPizzaRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyPizzaRestaurantApplication.class, args);
+	}
+
+}
