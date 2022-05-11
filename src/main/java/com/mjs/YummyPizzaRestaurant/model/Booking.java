bcd84a5b46f951582245d0ad5f5d715aec0e5f72
sean750317 <ken16592001@yahoo.com.tw>
2022-05-11 19:46:44 +1000

    getter and setter

--- src/main/java/com/mjs/YummyPizzaRestaurant/model/Booking.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/model/Booking.java
@@ -1,17 +1,18 @@
 package com.mjs.YummyPizzaRestaurant.model;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Booking {
     int bookingID;
-    LocalDateTime bookingDate; //Need booking date?
+    Date bookingDate; //Need booking date?
     int numberOfPerson;
-    LocalDateTime diningDateTime;
+    Date diningDateTime;
     int customerID;
     boolean groupBooking;
 
-    public Booking(int bookingID, LocalDateTime bookingDate, int numberOfPerson,
-                   LocalDateTime diningDateTime, int customerID, boolean groupBooking){
+    public Booking(int bookingID, Date bookingDate, int numberOfPerson,
+                   Date diningDateTime, int customerID, boolean groupBooking){
         this.bookingID = bookingID;
         this.bookingDate = bookingDate;
         this.numberOfPerson = numberOfPerson;
@@ -20,4 +21,51 @@ public class Booking {
         this.groupBooking = groupBooking;
     }
 
+    public int getBookingID() {
+        return bookingID;
+    }
+
+    public void setBookingID(int bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public int getNumberOfPerson() {
+        return numberOfPerson;
+    }
+
+    public void setNumberOfPerson(int numberOfPerson) {
+        this.numberOfPerson = numberOfPerson;
+    }
+
+    public Date getDiningDateTime() {
+        return diningDateTime;
+    }
+
+    public void setDiningDateTime(Date diningDateTime) {
+        this.diningDateTime = diningDateTime;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public boolean isGroupBooking() {
+        return groupBooking;
+    }
+
+    public void setGroupBooking(boolean groupBooking) {
+        this.groupBooking = groupBooking;
+    }
 }
--- src/main/java/com/mjs/YummyPizzaRestaurant/model/Customer.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/model/Customer.java
@@ -21,9 +21,59 @@ public class Customer {
         this.isMember = isMember;
     }
 
-    public int getCustomerID() {return customerID; }
-    public String getFirstName() {return firstName; }
-    public  String getLastName() {return lastName; }
+    public int getCustomerID() {
+        return customerID;
+    }
 
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void setMember(boolean member) {
+        isMember = member;
+    }
 }
