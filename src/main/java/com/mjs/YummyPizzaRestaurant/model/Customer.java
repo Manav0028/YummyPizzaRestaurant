17a7cc2e43a5be78a96e1d58974fe3e9448d247d
Manav Sharma <ishmanav@gmail.com>
2022-05-13 17:23:03 +1000

    customer model create

--- src/main/java/com/mjs/YummyPizzaRestaurant/model/Customer.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/model/Customer.java
@@ -1,4 +1,63 @@
 package com.mjs.YummyPizzaRestaurant.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long phone;
+    private String deliveryAddress;
+    private boolean isMember;
+
+    public Customer() {
+
+    }
+
+    public Customer(int customerId, String firstName, String lastName, String email, long phone, String deliveryAddress) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void setMember(boolean member) {
+        isMember = member;
+    }
+
 }
