a190c18f01875c9170141472dde1368e060e1533
Manav Sharma <ishmanav@gmail.com>
2022-05-13 17:28:40 +1000

    customer model create

--- src/main/java/com/mjs/YummyPizzaRestaurant/model/Payment.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/model/Payment.java
@@ -1,8 +1,13 @@
 package com.mjs.YummyPizzaRestaurant.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Payment {
+
+    @Id
     int paymentId;
     double paymentAmount;
     String paymentType;
