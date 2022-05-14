1e10483b4750e9969c1ef8138f74746ba0608c15
sean750317 <ken16592001@yahoo.com.tw>
2022-05-14 16:34:15 +1000

    Booking UI update

---
+++ src/main/java/com/mjs/YummyPizzaRestaurant/service/BookingService.java
@@ -0,0 +1,36 @@
+package com.mjs.YummyPizzaRestaurant.service;
+
+import com.mjs.YummyPizzaRestaurant.model.*;
+import com.mjs.YummyPizzaRestaurant.repo.BookingRepo;
+import com.mjs.YummyPizzaRestaurant.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+//@Service
+//public class BookingService {
+
+    //private Scanner scanner = new Scanner(System.in);
+
+    //@Autowired
+    //private BookingRepo bookings;
+
+    //@Autowired
+    //private CustomerRepo customers;
+
+    //private void Booking askNumberOfPerson() {
+        //System.out.println("Please enter number of people:");
+        //Integer numberOfPerson = scanner.nextInt();
+
+        //if (numberOfPerson > 20 || numberOfPerson < 6) {
+        //    System.out.println("Group booking only accepts a party size of 6 - 20, Please adjust the size.");
+        // } else {
+        //    this.booking = bookings.set(numberOfPerson);
+        //}
+
+   // }
+//}
+
+
+
