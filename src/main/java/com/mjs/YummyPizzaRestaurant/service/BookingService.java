7e8de49654161c7159605b6abd0e931e9dd9df47
sean750317 <ken16592001@yahoo.com.tw>
2022-05-16 17:13:44 +1000

    Booking UI - Check num of people

--- src/main/java/com/mjs/YummyPizzaRestaurant/service/BookingService.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/service/BookingService.java
@@ -5,32 +5,31 @@ import com.mjs.YummyPizzaRestaurant.repo.BookingRepo;
 import com.mjs.YummyPizzaRestaurant.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.Date;
 import java.util.Scanner;
 
-//@Service
-//public class BookingService {
+@Service
+public class BookingService {
 
-    //private Scanner scanner = new Scanner(System.in);
+    @Autowired
+    private BookingRepo bookings;
 
-    //@Autowired
-    //private BookingRepo bookings;
+    @Autowired
+    private CustomerRepo customers;
 
-    //@Autowired
-    //private CustomerRepo customers;
+//    Booking booking;
+//
+////    public void setupBooking(int numberOfPerson, int bookingDate, String customerName){
+////        booking = new Booking();
+////        System.out.println("jhdflk");
+////        booking.setNumberOfPerson(numberOfPerson);
+////        booking.setBookingDate(bookingDate);
+////        booking.setCustomerName(customerName);
+////        bookings.save(booking);
+////        System.out.println("gdhf");
+//    }
 
-    //private void Booking askNumberOfPerson() {
-        //System.out.println("Please enter number of people:");
-        //Integer numberOfPerson = scanner.nextInt();
-
-        //if (numberOfPerson > 20 || numberOfPerson < 6) {
-        //    System.out.println("Group booking only accepts a party size of 6 - 20, Please adjust the size.");
-        // } else {
-        //    this.booking = bookings.set(numberOfPerson);
-        //}
-
-   // }
-//}
+}
 
 
 
