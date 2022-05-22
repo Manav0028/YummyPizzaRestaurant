c66f05de1a15cb31fdc1085c404b2ccfaed01365
syafasofiena <syafa.sofiena@gmail.com>
2022-05-22 20:21:00 +1000

    final

--- src/main/java/com/mjs/YummyPizzaRestaurant/repo/BookingRepo.java
+++ src/main/java/com/mjs/YummyPizzaRestaurant/repo/BookingRepo.java
@@ -5,9 +5,6 @@ package com.mjs.YummyPizzaRestaurant.repo;
 import com.mjs.YummyPizzaRestaurant.model.Booking;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Date;
-import java.util.List;
-
 public interface BookingRepo extends CrudRepository<Booking, Integer> {
 
 
